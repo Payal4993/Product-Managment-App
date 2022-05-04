fb30f5b4078be99f86e1777ff52aed34cd9058c0
Payal4993 <prepayal4@gmail.com>
2022-05-04 12:55:03 +0530

    class commit

--- src/logicalprogramme/PalindromeString.java
+++ src/logicalprogramme/PalindromeString.java
@@ -1,5 +1,26 @@
 package logicalprogramme;
 
 public class PalindromeString {
+	
+	public void greet() {
+		
+		System.out.println("Welcome Payal");
+	}
+		
+	public void wish() {
+		
+		System.out.println("Welcome poonam");
+	}
+	
+	
+	
+		
+		
+		
+		
+	}
+	
+	
+	
 
-}
+
